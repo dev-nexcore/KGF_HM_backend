d3adf8ebc11e5f665c8107f49ae3c5255e46e2fc
mariyask04 <skmariya484@gmail.com>
2025-07-23 16:42:26 +0530

    student complaint and leave

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,5 +1,16 @@
 import express from 'express';
-import { login, forgotPassword, verifyOtp, resetPassword, checkInStudent, checkOutStudent } from '../controllers/student.controller.js';
+import { 
+    login, 
+    forgotPassword, 
+    verifyOtp, 
+    resetPassword, 
+    checkInStudent, 
+    checkOutStudent, 
+    fileComplaint, 
+    getComplaintHistory, 
+    applyForLeave,
+    getLeaveHistory
+} from '../controllers/student.controller.js';
 
 const router = express.Router();
 
@@ -9,6 +20,9 @@ router.post('/verify-otp', verifyOtp);
 router.post('/reset-password', resetPassword);
 router.post('/check-in', checkInStudent);
 router.post('/check-out', checkOutStudent);
-
+router.post("/complaint", fileComplaint);
+router.get("/complaints/:studentId", getComplaintHistory);
+router.post("/leave", applyForLeave);
+router.get("/leaves/:studentId", getLeaveHistory);
 
 export default router;
