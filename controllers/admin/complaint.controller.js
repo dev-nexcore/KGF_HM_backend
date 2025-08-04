491c4a5463b298ad194d27f1b07153c06474b266
Resource-02 <resource2.code4bharat@gmail.com>
2025-08-04 07:56:20 +0530

    update complaint.controller.js

--- controllers/admin/complaint.controller.js
+++ controllers/admin/complaint.controller.js
@@ -4,7 +4,7 @@ import { Complaint } from '../../models/complaint.model.js';
 import { createAuditLog, AuditActionTypes } from '../../utils/auditLogger.js';
 
 // configure SMTP transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
     host:    process.env.MAIL_HOST,      // smtp.gmail.com
   port:   +process.env.MAIL_PORT,      // 587
   secure: process.env.MAIL_SECURE === 'true',
